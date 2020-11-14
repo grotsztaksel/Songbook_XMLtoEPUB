75fdcd633fe520140adb2636577f204d4dd8889a
grotsztaksel <grotsztaksel@o2.pl>
2020-11-14 21:43:28 +0100

    Add ChordMode(Enum)
    # Conflicts:
    #	scripts/config/__init__.py
    #	scripts/config/config.py

---
+++ scripts/config/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 14 18:18:26 2020
+
+@author: piotr
+"""
+
+__all__ = ['CFG', 'ChordMode']
+from .config import CFG, ChordMode
---
+++ scripts/config/config.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 14 17:32:27 2020
+
+@author: piotr
+"""
+
+__all__ = ['ChordMode', 'CFG']
+
+import os
+from enum import Enum
+
+class ChordMode(Enum):
+    """
+    CHORD_ABOVE - the chords will be written above the text, indicating the place, where the chord should be played
+    CHORD_BESIDE - the chords will be listed to the right of the line
+    NO_CHORD - the chords will not be written alltogether
+    """
+    CHORDS_ABOVE = 0
+    CHORDS_BESIDE = 1
+    NO_CHORDS = 2
+
+class config():
+    ROOT_DIR = os.path.abspath("../")
+    OUTPUT_DIR = os.path.join(ROOT_DIR, "output")    
+    SONG_SRC_DIR = os.path.join(ROOT_DIR, "song_src")
+    SONG_SRC_XML = os.path.join(SONG_SRC_DIR, "songs_src.xml")
+    SONG_HTML_DIR = os.path.join(OUTPUT_DIR, "text")    
+    MODE = ChordMode.CHORDS_ABOVE
+    
+
+        
+
+CFG = config()
