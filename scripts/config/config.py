bf86ac2f8756a5fa653bbddcaf56a9ee94dbfb47
grotsztaksel <grotsztaksel@o2.pl>
2020-11-16 22:30:19 +0100

    Define the chord characters as constants ">" and "|"

--- scripts/config/config.py
+++ scripts/config/config.py
@@ -10,6 +10,7 @@ __all__ = ['ChordMode', 'CFG']
 import os
 from enum import Enum
 
+
 class ChordMode(Enum):
     """
     CHORD_ABOVE - the chords will be written above the text, indicating the place, where the chord should be played
@@ -20,15 +21,18 @@ class ChordMode(Enum):
     CHORDS_BESIDE = 1
     NO_CHORDS = 2
 
+
 class config():
     ROOT_DIR = os.path.abspath("../")
-    OUTPUT_DIR = os.path.join(ROOT_DIR, "output")    
+    OUTPUT_DIR = os.path.join(ROOT_DIR, "output")
     SONG_SRC_DIR = os.path.join(ROOT_DIR, "song_src")
     SONG_SRC_XML = os.path.join(SONG_SRC_DIR, "songs_src.xml")
-    SONG_HTML_DIR = os.path.join(OUTPUT_DIR, "text")    
+    SONG_HTML_DIR = os.path.join(OUTPUT_DIR, "text")
     MODE = ChordMode.CHORDS_BESIDE
-    
 
-        
+    CS = ">"  # [C]hord [S]eparator: character that separates the text from chords lists
+    CI = "|"  # [C]hord [I]nsertion point: character indicating the location where the chord should be changed
+    #                                      while performing the song
+
 
 CFG = config()
