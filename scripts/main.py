40837fa56d2128c8e584a7176a841f89db489d0f
grotsztaksel <grotsztaksel@o2.pl>
2020-11-16 19:59:55 +0100

    Clear directory SONG_HTML_DIR before writing song xhtml files
    # Conflicts:
    #	scripts/main.py

--- scripts/main.py
+++ scripts/main.py
@@ -5,23 +5,53 @@ Spyder Editor
 This is a temporary script file.
 """
 
-from tixi3.tixi3wrapper import Tixi3 as Tixi
+from tixi import Tixi, TixiException
 import os
 import sys
+import shutil
+from config import CFG
 
-class config(object):
-    def __init__(self):
-        super(config,self).__init__()
-        self.outputDir = os.path.abspath("../output")
+from tools import HtmlWriter
 
+
+def write_songs(max_n_songs=None):
+    """
+    Read the source file and for each song defined, write a properly formatted
+    song xml file in the required location
+    """
+    tixi = Tixi()
+    tixi.open(CFG.SONG_SRC_XML, recursive=True)
+    tixi.registerNamespace("http://www.w3.org/2001/XMLSchema-instance", 'xsd')
+    
+    xPath="//song[@title]"
+    try:
+        n = tixi.xPathEvaluateNodeNumber(xPath)
+    except TixiException:
+        n = 0
+        
+    print ("Found {} songs".format(n))
+    
+    
+    for i in range(1, min(n+1, max_n_songs)):
+        xmlPath = tixi.xPathExpressionGetXPath(xPath, i)
+        writer = HtmlWriter(tixi, xmlPath)
+        writer.write_song_file()
+    
 
 def main(argv):
-    
-    cfg = config()
-    print(cfg.outputDir)
+    sys.excepthook = print_exceptions
+    shutil.rmtree(CFG.SONG_HTML_DIR, ignore_errors=True)
+    os.mkdir(CFG.SONG_HTML_DIR)
+    write_songs(10)
     
 
+def print_exceptions(etype, value, tb):
+    import sys
+    import traceback
+    from PyQt5.QtWidgets import QMessageBox
     
+    text = "\n".join(traceback.format_exception(etype, value, tb))
+    QMessageBox.critical(None, "Python Error!", text)
 
 
 main(sys.argv)
