1fa78f7ce9dda12c732acea8a5d244e986e28ee5
grotsztaksel <grotsztaksel@o2.pl>
2020-11-16 22:31:31 +0100

    Add test_html_writer.py

---
+++ scripts/test/tools/test_html_writer.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 16.11.2020 22:14
+ 
+@author: piotr
+"""
+
+import unittest
+
+from config import CFG
+from tools.html_writer import HtmlWriter, LineWithChords
+
+
+class TestHtmlWriter(unittest.TestCase):
+    def test_identifyLinesWithChords(self):
+        text = "\n".join(["Line 1",
+                          "Line 2",
+                          "Line 3",
+                          "Line 4"])
+
+        # Should basically get the text with <br/> tags in newlines
+        self.assertEqual([text.replace("\n", "<br/>\n")], HtmlWriter._identifyLinesWithChords(text))
+
+        text = "\n".join(["Line 1" + CFG.CS + "F E E D",
+                          "Line 2" + CFG.CS + "F E E D",
+                          "Line 3",
+                          "Line 4"])
+        expected = [
+            LineWithChords("Line 1", ["F E E D"]),
+            LineWithChords("Line 2", ["F E E D"]),
+            "Line 3<br/>\nLine 4"
+        ]
+        self.assertEqual(expected, HtmlWriter._identifyLinesWithChords(text))
+
+
+if __name__ == '__main__':
+    unittest.main()
