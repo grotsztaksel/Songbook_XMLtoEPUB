1df5f23519598da16c0143a929eaed4366ed5706
grotsztaksel <grotsztaksel@o2.pl>
2020-11-20 21:03:48 +0100

    Add test for the xsd schema

---
+++ scripts/test/config/test_schema.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 20.11.2020 20:42
+ 
+@author: piotr
+"""
+
+import os
+import unittest
+
+from config import config
+from tixi import Tixi, TixiException, ReturnCode
+
+
+class TestSchema(unittest.TestCase):
+    def setUp(self):
+        self.xsdpath = os.path.join(os.path.dirname(os.path.abspath(config.__file__)), "source_schema.xsd")
+
+    def test_xsd(self):
+        """Check if the XSD file is there at all and can be opened"""
+        self.assertTrue(os.path.isfile(self.xsdpath))
+        tixi = Tixi()
+        tixi.openDocument(self.xsdpath)
+
+    def test_xsd_valid(self):
+        """Check if the XSD file can even be used for validation"""
+        tixi = Tixi()
+        string = '<?xml version="1.0"?><root><child/></root>'
+        tixi.openString(string)
+
+        self.assertNonSchemaCompliant(tixi)
+
+    def test_empty_src(self):
+        tixi = Tixi()
+        tixi.create("songbook")
+        tixi.registerNamespace("http://www.w3.org/2001/XMLSchema", "xsd")
+
+        tixi.schemaValidateFromFile(self.xsdpath)
+
+    def test_cascade_of_chapters(self):
+        tixi = Tixi()
+        tixi.create("songbook")
+        tixi.registerNamespace("http://www.w3.org/2001/XMLSchema", "xsd")
+
+        path = "/songbook"
+        for i in range(4):
+            tixi.createElement(path, "section")
+            path += "/section"
+            tixi.addTextAttribute(path, "title", "someTitle")
+            tixi.schemaValidateFromFile(self.xsdpath)
+
+        tixi.createElement(path, "song")
+        tixi.addTextAttribute(path + "/song", "title", "Song Title")
+        tixi.schemaValidateFromFile(self.xsdpath)
+
+        # Should be illegal to place a song AND section on the same level
+        tixi.createElement(path, "section")
+        tixi.addTextAttribute(path + "/section", "title", "Illegal section title")
+
+        self.assertNonSchemaCompliant(tixi)
+
+    def assertNonSchemaCompliant(self, tixi):
+        try:
+            tixi.schemaValidateFromFile(self.xsdpath)
+        except TixiException as e:
+            self.assertEqual(ReturnCode.NOT_SCHEMA_COMPLIANT, e.code)
+
+
+if __name__ == '__main__':
+    unittest.main()
