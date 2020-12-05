9252d0d49a5a2be70325ba8525d0dd5bcb960c2e
grotsztaksel <grotsztaksel@o2.pl>
2020-12-05 22:37:19 +0100

    Expand TestSongWriter and add reference test files

--- scripts/test/tools/test_song_writer.py
+++ scripts/test/tools/test_song_writer.py
@@ -7,11 +7,22 @@ Created on 16.11.2020 22:14
 
 import unittest
 
-from config import CFG
+from config import EpubSongbookConfig
+from tixi import Tixi, TixiException, ReturnCode
 from tools.song_writer import SongWriter, LineWithChords
 
 
 class TestSongWriter(unittest.TestCase):
+    def setUp(self):
+        # Need a Tixi object to initialize the EpubSongbookConfig
+        cfgTixi = Tixi()
+        self.settings = EpubSongbookConfig(cfgTixi)
+        self.settings.CS = ">"
+        self.settings.CI = "|"
+
+    def test_write_chors_above(self):
+        pass
+
     def test_identifyLinesWithChords(self):
         text = "\n".join(["Line 1",
                           "Line 2",
@@ -21,8 +32,8 @@ class TestSongWriter(unittest.TestCase):
         # Should basically get the text with <br/> tags in newlines
         self.assertEqual([text.replace("\n", "<br/>\n")], SongWriter._identifyLinesWithChords(text))
 
-        text = "\n".join(["Line 1" + CFG.CS + "F E E D",
-                          "Line 2" + CFG.CS + "F E E D",
+        text = "\n".join(["Line 1" + self.settings.CS + "F E E D",
+                          "Line 2" + self.settings.CS + "F E E D",
                           "Line 3",
                           "Line 4"])
         expected = [
