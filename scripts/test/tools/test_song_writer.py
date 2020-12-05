50b19a28c5ae65bef11abfb690c18c11ccd4d5de
grotsztaksel <grotsztaksel@o2.pl>
2020-12-05 23:29:59 +0100

    Cleanup test_write_chors_above till it exposes the actual failure

--- scripts/test/tools/test_song_writer.py
+++ scripts/test/tools/test_song_writer.py
@@ -16,12 +16,47 @@ class TestSongWriter(unittest.TestCase):
     def setUp(self):
         # Need a Tixi object to initialize the EpubSongbookConfig
         cfgTixi = Tixi()
+        cfgTixi.create("songbook")
         self.settings = EpubSongbookConfig(cfgTixi)
         self.settings.CS = ">"
         self.settings.CI = "|"
 
     def test_write_chors_above(self):
-        pass
+        src_tixi = Tixi()
+        src_tixi.open("test_song.xml")
+        writer = SongWriter(src_tixi, self.settings, "/song")
+        writer.tixi.createElement("/html", "body")
+        # "/html" is ok, because the function doesn't really check where the target is
+        writer.write_chords_above("/song/verse[1]", "/html/body")
+
+        expectedTixi = Tixi()
+        expectedTixi.open("expected_test_song.xhtml")
+        expectedTixi.registerNamespace("http://www.w3.org/1999/xhtml", "x")
+        # Need to trim it a little - don't want things we're not checking
+        expectedTixi.removeElement("/x:html/x:body/x:h1")
+        self.assertTrue(expectedTixi.checkAttribute("/x:html/x:body/x:p[1]", "class"))
+        self.assertEqual("authors", expectedTixi.getTextAttribute("/x:html/x:body/x:p[1]", "class"))
+        expectedTixi.removeElement("/x:html/x:body/x:p[1]")
+
+        self.assertIn("Now a verse without any chords", expectedTixi.getTextElement("/x:html/x:body/x:p[2]"))
+        expectedTixi.removeElement("/x:html/x:body/x:p[2]")
+        # Now removing the last <p/> without checking!
+        expectedTixi.removeElement("/x:html/x:body/x:p[2]")
+        self.assertEqual(1, expectedTixi.getNamedChildrenCount("/x:html/x:body", "x:p"))
+        expectedTixi.removeAttribute("/x:html/x:body/x:p", "class")
+        texts = []
+
+        td_all = expectedTixi.getPathsFromXPathExpression("//x:td")
+        td_txt = expectedTixi.getPathsFromXPathExpression("//x:td[text()]")
+
+        for path in td_all:
+            if path in td_txt:
+                continue
+            expectedTixi.updateTextElement(path, "")
+
+        # If we're still here, the expectedTixi has been properly formed.
+        self.assertEqual(expectedTixi.exportDocumentAsString(),
+                         writer.tixi.exportDocumentAsString())
 
     def test_identifyLinesWithChords(self):
         text = "\n".join(["Line 1",
