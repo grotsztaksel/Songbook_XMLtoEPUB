4ac0cdbc1fb31817916ab1f6edc0fe941cddfce3
grotsztaksel <grotsztaksel@o2.pl>
2021-01-23 17:43:45 +0100

    Expand test_findSOngsByAuthor

--- scripts/test/tools/test_index_authors_writer.py
+++ scripts/test/tools/test_index_authors_writer.py
@@ -16,18 +16,48 @@ class TestAuthorsWriter(unittest.TestCase):
     def setUp(self):
         self.src_file = os.path.join(os.path.dirname(__file__), "test_song_src.xml")
         tixi = Tixi()
-        tixi.open(self.src_file)
+        tixi.open(self.src_file, recursive=True)
         self.settings = EpubSongbookConfig(tixi)
+        # Need to add fake xhtml attributes, otherwise the AuthorsWriter will bomb out
+        i = 0
+        for path in tixi.getPathsFromXPathExpression("//song"):
+            i += 1
+            tixi.addTextAttribute(path, "xhtml", "song_file_{}.xhtml".format(i))
+
         self.writer = AuthorsWriter(tixi, self.settings)
 
     def test_findSOngsByAuthor(self):
         # The method should've been called upon initialization
         expected_std_author_names = {"John Doe": "Doe, John",
-                  "Mike Moo": "Moo, Mike",
-                  "P. Gradkowski": "Gradkowski, P",
-                  "Sam Composer": "Composer, Sam"
-                  }
-        self.assertEqual(expected_std_author_names, self.writer.standardize_author_names)
+                                     "Mike Moo": "Moo, Mike",
+                                     "P. Gradkowski": "Gradkowski, P.",
+                                     "Sam Composer": "Composer, Sam",
+                                     'The Developers': 'The Developers'
+                                     }
+        self.assertEqual(expected_std_author_names, self.writer.standardized_author_names)
+
+        expected_songs_dict = {
+            "Doe, John": {
+                "Song A": "song_file_3.xhtml",
+                "Song B": "song_file_4.xhtml",
+                "Song C": "song_file_5.xhtml",
+                "Song ABBA": "song_file_9.xhtml"
+            },
+            "Moo, Mike": {
+                "Song C": "song_file_5.xhtml",
+                "Song A": "song_file_6.xhtml"
+            },
+            "Gradkowski, P.": {
+                "My Test Song": "song_file_2.xhtml"
+            },
+            "Composer, Sam": {
+                "My Test Song": "song_file_2.xhtml"
+            },
+            'The Developers': {
+                "My Test Song": "song_file_2.xhtml"
+            }
+        }
+        self.assertEqual(expected_songs_dict, self.writer.songs_by_author)
 
     def test_standardize_author_name(self):
         self.assertEqual("Kowalski, M.", AuthorsWriter.standardize_author_name("M.Kowalski"))
