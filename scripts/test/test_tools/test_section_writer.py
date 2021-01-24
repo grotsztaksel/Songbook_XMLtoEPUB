ceb67648104df936e58ea074ec24f88c1bd467ad
grotsztaksel <grotsztaksel@o2.pl>
2021-01-24 11:57:00 +0100

    Add missing test in TestSectionWriter

--- scripts/test/test_tools/test_section_writer.py
+++ scripts/test/test_tools/test_section_writer.py
@@ -21,6 +21,56 @@ class TestSectionWriter(unittest.TestCase):
         self.sg.tixi = tixi
         self.sg.getBasicSongInfo()
         self.tixi = self.sg.tixi
+        self.test_output = os.path.join(os.path.dirname(__file__), "test_section.xml")
+
+    def tearDown(self):
+        if os.path.isfile(self.test_output):
+            os.remove(self.test_output)
+
+    def test_write_section_file(self):
+        expected = """<?xml version="1.0"?>
+        <html xmlns="http://www.w3.org/1999/xhtml">
+          <head>
+            <title>My Songbook</title>
+            <link rel="stylesheet" type="text/css" href="../songbook.css"/>
+          </head>
+          <body>
+            <h2>Section 1</h2>
+            <p>
+              <ul>
+                <li>
+                  <a href="sec_section_1.1.xhtml">Section 1.1</a>
+                  <ul>
+                    <li>
+                      <a href="sng_song_a.xhtml">Song A</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="sec_section_1.2.xhtml">Section 1.2</a>
+                  <ul>
+                    <li>
+                      <a href="sng_song_b.xhtml">Song B</a>
+                    </li>
+                    <li>
+                      <a href="sng_song_c.xhtml">Song C</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </p>
+          </body>
+        </html>""".replace("        <","<").strip()
+
+        sw = SectionWriter(self.tixi, self.sg.settings, "/songbook/section[1]")
+        sw.write_section_file(self.test_output)
+
+        self.assertTrue(os.path.isfile(self.test_output))
+        with open(self.test_output, "r") as f:
+            content = f.read()
+
+        self.assertEqual(expected, content.strip())
+
 
     def test_write_html_header(self):
         sw = SectionWriter(self.tixi, self.sg.settings, "/songbook/section[1]")
