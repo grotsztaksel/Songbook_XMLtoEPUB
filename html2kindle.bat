a5e08210c29dce6f9147c33d475f43cf6d2a8bf4
grotsztaksel <grotsztaksel@o2.pl>
2021-06-05 23:06:39 +0200

    Move the bat file

---
+++ html2kindle.bat
@@ -0,0 +1,6 @@
+7z a -r songbook.zip output 
+move songbook.zip songbook.epub
+ebook-convert songbook.epub songbook.azw3  --duplicate-links-in-toc  --max-toc-links=0
+
+ebook-viewer songbook.azw3
+@pause
