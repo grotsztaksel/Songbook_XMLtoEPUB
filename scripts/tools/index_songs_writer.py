03f3a5465048ca4cbdcfb9bb6f340501a6f05caa
grotsztaksel <grotsztaksel@o2.pl>
2021-06-20 17:18:01 +0200

    SongsIndexWriter: Use sorted list of tuples (title, file) instead of a dictionary

--- scripts/tools/index_songs_writer.py
+++ scripts/tools/index_songs_writer.py
@@ -20,31 +20,26 @@ class SongsIndexWriter(HtmlWriter):
     def __init__(self, tixi: Tixi, settings: EpubSongbookConfig):
         super(SongsIndexWriter, self).__init__(tixi, settings)
 
-        self.songs = dict()
+        self.songs = list()
 
         self.getListOfSongs()
 
     def getListOfSongs(self):
-        """Build a list containing song titles and the songs' html file names.
-            Create a dictionary with filenames as keys
-            1. Need to sort vs song titles
-            2. Song titles are not unlikely to be non-unique
-            3. Because of (1-2), cannot use the song titles as dictionary keys
-            4. Filenames are unique
-            5. Filenames are previously built from song titles, so they should be sortable.
+        """
+        Build a list containing song titles and the songs' html file names.
         """
         for path in self.src_tixi.xPathExpressionGetAllXPaths("//song[@title and @xhtml]"):
             title = self.src_tixi.getTextAttribute(path, "title")
             file = self.src_tixi.getTextAttribute(path, "xhtml")
-            self.songs[file] = title
+            self.songs.append((title, file))
+        self.songs.sort(key=lambda x: x[0])
 
     def write_index(self):
         bPath = self.tixi.createElement("/html", "body")
         self.tixi.addTextElement(bPath, "h2", self.settings.alphabetical_index_title)
 
         I = ""  # Initial
-        for file in sorted(self.songs.keys()):
-            title = self.songs[file]
+        for title, file in self.songs:
             if title[0] > I:
                 I = title[0]
                 self.tixi.addTextElement(bPath, "h3", I)
@@ -54,3 +49,9 @@ class SongsIndexWriter(HtmlWriter):
             liPath = self.tixi.createElement(ulPath, "li")
             aPath = self.tixi.addTextElement(liPath, "a", title)
             self.tixi.addTextAttribute(aPath, "href", file)
+
+    def sort_songs(self):
+        """
+        Return list of tuple pairs: (title, filename), sorted by the title
+
+        """
