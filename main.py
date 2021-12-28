4f4ea72585b328b9660684b0e938c1584afe2b23
grotsztaksel <grotsztaksel@o2.pl>
2021-12-28 20:45:52 +0100

    Use argparser

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ Created on 14.11.2020 17:05
 __authors__ = ['Piotr Gradkowski <grotsztaksel@o2.pl>']
 __date__ = '2020-11-14'
 
-import os
-import sys
+import argparse
 import logging
+import os
+import pathlib
+import sys
 
 try:
     # Enable importing modules from PATH environmental variable (Python 3.8+ on Windows)
@@ -20,12 +22,17 @@ except AttributeError:
 
 from scripts.tools.song_book_generator import SongBookGenerator
 
-logfile = "ebook_generator.log"
 
-
-def main(argv):
+def main(argparser: argparse.ArgumentParser):
     sys.excepthook = print_exceptions
-    logging.basicConfig(level=logging.INFO,
+    args = argparser.parse_args()
+    logfile = args.logfile.buffer.raw.name
+    loglevel = {'critical': logging.CRITICAL,
+                'error': logging.ERROR,
+                'warning': logging.WARNING,
+                'info': logging.INFO,
+                'debug': logging.DEBUG}[args.loglevel]
+    logging.basicConfig(level=loglevel,
                         format="%(asctime)s [%(levelname)s] %(message)s",
                         handlers=[
                             logging.FileHandler(logfile, 'w', 'utf-8'),
@@ -34,13 +41,10 @@ def main(argv):
                         )
     print("Logging information to {}".format(os.path.abspath(os.path.join(os.getcwd(), logfile))))
 
-    if len(argv) < 2:
-        raise IOError("Need an XML file name as input!")
-
     xsd_file = os.path.join(os.path.dirname(__file__), "config", "source_schema.xsd")
     if not os.path.isfile(xsd_file):
         xsd_file = None
-    sg = SongBookGenerator(argv[1], xsd_file)
+    sg = SongBookGenerator(args.input.buffer.raw.name, xsd_file)
     sg.write_songs()
     sg.write_sections()
     sg.write_indexes()
@@ -57,7 +61,21 @@ def print_exceptions(etype, value, tb):
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    parser = argparse.ArgumentParser(description="Generate a songbook e-Book from XML files")
+    parser.add_argument('input', type=argparse.FileType('r'),
+                        help="Name of the input XML file")
+    parser.add_argument('--logfile', type=argparse.FileType('w'), default="ebook_generator.log",
+                        help="Name of the log file")
+    parser.add_argument('--loglevel', type=str, choices=['critical', 'error', 'warning', 'info', 'debug'],
+                        default='info',
+                        help="Level of logged information")
+
+    parser.add_argument('--postproc', type=str,
+                        help="Name of the postprocessing script. For example a script that will automatically zip the "
+                             "output directory (this tool does not do that by itself)")
+    parser.add_argument('--postwd', type=str, default=os.getcwd(),
+                        help="Working directory where the postprocessing script should be launched")
+    main(parser)
     from subprocess import Popen
 
     bat = "html2kindle.bat"
