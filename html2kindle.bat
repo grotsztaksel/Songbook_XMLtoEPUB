1402f84448a25c3d7288ffbc9d453e4090759391
grotsztaksel <grotsztaksel@o2.pl>
2024-06-28 18:33:39 +0200

    Expand my script

--- html2kindle.bat
+++ html2kindle.bat
@@ -1,5 +1,10 @@
 7z a -r songbook.zip output 
 move songbook.zip songbook.epub
 ebook-convert songbook.epub songbook.azw3  --duplicate-links-in-toc  --max-toc-links=0
+ebook-convert songbook.epub songbook.pdf  --duplicate-links-in-toc  --max-toc-links=0
+
+copy /y songbook.epub C:\Users\piotr\OneDrive\Books\Songbook
+copy /y songbook.pdf C:\Users\piotr\OneDrive\Books\Songbook
+copy /y songbook.pdf C:\Users\piotr\OneDrive\Books\Songbook
 
 ebook-viewer songbook.azw3
